cb00f8fafe2372fbbe744d9bf4256188b5328777
Sagar Chaudhari <sagarchaudhari2202@gmail.com>
2024-01-02 19:08:25 +0530

    PortFolio

---
+++ Portfilio/lib/Presentation/Screens/Homepage/Widgets/profilePage/sliver_backgroundImage.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class SliverBackgroundImage extends StatelessWidget {
+  const SliverBackgroundImage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      elevation: 0,
+      pinned: false,
+      centerTitle: false,
+      stretch: true,
+      expandedHeight: 180.0,
+      flexibleSpace: const FlexibleSpaceBar(
+        title: Text("Sagar Chaudhari",
+                style: TextStyle(
+                fontSize: 22,
+                color: Color.fromARGB(255, 212, 255, 0)
+              ),
+        ),
+        centerTitle: true,
+        stretchModes: [StretchMode.zoomBackground],
+        collapseMode: CollapseMode.parallax,
+        background: Image(
+          image: AssetImage('assets/Images/BackGroup.jpg'),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
